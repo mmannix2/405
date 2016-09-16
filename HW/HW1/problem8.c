c8efd723c93fbac2f0059d6690d096f5f022f22d
Matthew Mannix <m.mannix3@gmail.com>
2016-09-16 13:40:52 -0400

    Add problem8.c.

---
+++ HW/HW1/problem8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char** argv) {
+    printf("***Problem #8***\n");
+    
+    //TODO Do the assignment
+    
+    return 0;
+}
